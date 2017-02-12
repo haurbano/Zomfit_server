461d782664696a7286f1070423af4d995441d46d
Hamilton Urbano <h.a.u.r1993@gmail.com>
2017-02-12 14:54:16 -0500

    Added authy fitbit

--- index.js
+++ index.js
@@ -4,11 +4,33 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var events = require('./events.js')
+var passport = require('passport');
+var FitbitStrategy = require('passport-fitbit-oauth2').FitbitOAuth2Strategy;;
+
+passport.use(new FitbitStrategy({
+    clientID:     "2284L5",
+    clientSecret: "da0c45dcf15e8308d6c56e91df9a8648",
+    callbackURL: "http://mifuturoya.com:3000/auth/fitbit/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    User.findOrCreate({ fitbitId: profile.id }, function (err, user) {
+      return done(err, user);
+    });
+  }
+));
 
 app.set('views', __dirname + '/static/views');
 app.set('view engine', 'jade');
 app.use(express.static(__dirname + '/static'));
 
+app.get('/auth/fitbit',
+  passport.authenticate('fitbit', { scope: ['activity','heartrate','location','profile'] }
+));
+
+app.get( '/auth/fitbit/callback', passport.authenticate( 'fitbit', {
+        successRedirect: '/auth/fitbit/success',
+        failureRedirect: '/auth/fitbit/failure'
+}));
 
 app.get('/', function(req, res){
     res.render('index',{pageTitle:"Tesis :D", players: events.players});
