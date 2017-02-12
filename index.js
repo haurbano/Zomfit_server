e177b275f9bb30d32b1ae6799140518cd2fcb249
Hamilton Urbano <h.a.u.r1993@gmail.com>
2017-02-12 14:59:27 -0500

    Url changed

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var FitbitStrategy = require('passport-fitbit-oauth2').FitbitOAuth2Strategy;;
 passport.use(new FitbitStrategy({
     clientID:     "2284L5",
     clientSecret: "da0c45dcf15e8308d6c56e91df9a8648",
-    callbackURL: "http://mifuturoya.com:3000/auth/fitbit/callback"
+    callbackURL: "h54.187.11.142:3000/auth/fitbit/callback"
   },
   function(accessToken, refreshToken, profile, done) {
     User.findOrCreate({ fitbitId: profile.id }, function (err, user) {
